a3fe37de01b69c905bdef7f2ba86632293f839af
dvauquelin <david.vauquelin@planisware.com>
2022-11-15 15:04:55 +0100

    0. Init

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,4 @@ require('@nomicfoundation/hardhat-toolbox');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.1',
-  networks: {
-    hardhat: {
-      allowUnlimitedContractSize: true,
-    },
-  },
 };
